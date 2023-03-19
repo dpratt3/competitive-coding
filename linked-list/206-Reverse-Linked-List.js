696c826007236474b77af2764b3673b06fed360f
David Pratt <david@midnightmechanism.com>
2023-03-19 12:56:31 -0700

    Reverse linked list

---
+++ linked-list/206-Reverse-Linked-List.js
@@ -0,0 +1,37 @@
+/*
+206. Reverse Linked List
+19 March 2023
+https://leetcode.com/problems/reverse-linked-list/
+David Pratt
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// iterative
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+
+    while(curr){
+        let forward = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = forward;
+    }
+
+    return prev;   
+};
+
+// recursive approach: https://www.youtube.com/watch?v=G0_I-ZF0S38
+
+
