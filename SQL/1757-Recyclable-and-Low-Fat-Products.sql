7fa29d449d47f29da76444218bfe8e54a4471e77
David Pratt <david@midnightmechanism.com>
2023-03-26 15:39:26 -0700

    Solve 1757

---
+++ SQL/1757-Recyclable-and-Low-Fat-Products.sql
@@ -0,0 +1,8 @@
+1757. Recyclable and Low Fat Products
+# 26 March 2023
+# https://leetcode.com/problems/big-countries/description/?envType=study-plan&id=sql-i
+# David Pratt
+
+# Write your MySQL query statement below
+
+SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
