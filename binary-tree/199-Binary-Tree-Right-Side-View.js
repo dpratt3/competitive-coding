eef92b99b63e036539fa060c75227bf9afcf76ce
David Pratt <david@midnightmechanism.com>
2023-03-27 14:36:38 -0700

    Solve No. 199 binary tree right view

---
+++ binary-tree/199-Binary-Tree-Right-Side-View.js
@@ -0,0 +1,40 @@
+/*
+199. Binary Tree Right Side View
+27 March 2023
+https://leetcode.com/problems/binary-tree-right-side-view/
+David Pratt
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if(!root) return [];
+    let queue = [root]
+    let visited = []
+    
+    while(queue.length){
+        const levLen = queue.length;
+        const levNodes = [];
+        for(let i = 0; i < levLen; i += 1){
+            let curr = queue.shift()
+            levNodes.push(curr.val)
+            if(curr.left) queue.push(curr.left)
+            if(curr.right) queue.push(curr.right)
+         }
+         visited.push(levNodes)
+    }
+
+    // same as return array of arrays representing levels
+    return visited.map(arr => arr[arr.length - 1]);
+};
