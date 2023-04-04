01023484e16accaa7d0825db5e69bd434b8e9043
David Pratt <david@midnightmechanism.com>
2023-04-04 13:55:58 -0500

    Correct town judge problem

--- graph/997-Find-the-Town-Judge.js
+++ graph/997-Find-the-Town-Judge.js
@@ -1,6 +1,6 @@
 /*
 997. Find the Town Judge
-3 April 2023
+4 April 2023
 https://leetcode.com/problems/find-the-town-judge/
 David Pratt
 */
@@ -10,9 +10,44 @@ David Pratt
  * @param {number[][]} trust
  * @return {number}
  */
-var findJudge = function(n, trust) {
-    const trustJudge = (trust.filter(x => x[1] === n).length === trust.length)
-    const judgeTrust = (trust.filter(x => x[0] === n).length === 0)
-    if(trustJudge && judgeTrust) return n
-    return -1
+
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+
+const adjListMaker = (trust) => {
+  let trustObj = {};
+  trust.map((x) => {
+    [key, value] = x;
+    if (!trustObj[key]) trustObj[key] = [value];
+    else trustObj[key].push(value);
+  });
+  return trustObj;
+};
+
+var findJudge = function (n, trust) {
+  // handle edge cases
+  if (trust.length === 0 && n > 1) return -1;
+  if (n === 1) return 1;
+
+  const trustObj = adjListMaker(trust);
+
+  // make sure the judge doesn't trust anyone; get candidate judge
+  let judgeCompliment = Object.keys(trustObj).map((x) => Number(x));
+  let candidate;
+  for (let i = 1; i <= n; i += 1) {
+    if (!judgeCompliment.includes(i)) candidate = i;
+  }
+  console.log(candidate);
+  let count = 0;
+
+  for (key of Object.keys(trustObj)) {
+    if (trustObj[key].includes(candidate)) count += 1;
+  }
+
+  // if the candidate is in every array, it is the judge
+  if (count === judgeCompliment.length) return candidate;
+  return -1;
 };
