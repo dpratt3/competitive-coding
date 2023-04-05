78ad43234e5cc5b832118f85386e9af5afb0f30c
David Pratt <david@midnightmechanism.com>
2023-04-05 16:26:29 -0700

    Finish move zeroes

---
+++ array/283-Move-Zeroes.js
@@ -0,0 +1,26 @@
+/*
+283. Move Zeroes
+https://leetcode.com/problems/move-zeroes/
+5 April 2023
+David Pratt
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var moveZeroes = function(nums) {
+    let zeroCount = 0
+    for(let i = 0; i < nums.length; i+=1){
+         if(nums[i] === 0){
+             nums.splice(i, 1);
+             zeroCount += 1
+             i--;
+         }    
+    }
+    for(let i = 0; i < zeroCount; i+=1){
+        nums.push(0)
+    }
+    return nums
+};
