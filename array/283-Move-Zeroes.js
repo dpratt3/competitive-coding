5853a78488031f533a9f2b26159d3b3225ab68ee
David Pratt <david@midnightmechanism.com>
2023-04-05 17:05:40 -0700

    Update move zeroes

--- array/283-Move-Zeroes.js
+++ array/283-Move-Zeroes.js
@@ -22,5 +22,23 @@ var moveZeroes = function(nums) {
     for(let i = 0; i < zeroCount; i+=1){
         nums.push(0)
     }
-    return nums
+    return nums // felt like returning anyway
 };
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let zeroCount = 0
+    for(let i = 0; i < nums.length; i+=1){
+         if(nums[i] === 0){
+             let val = nums.splice(i, 1);
+             i--;
+             nums.push(val)
+         }    
+    }
+    return nums // felt like returning anyway
+};
+
+
