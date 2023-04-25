cbf76e46d8dc327a87f0a45c1edcfa95000d580d
David Pratt <david@midnightmechanism.com>
2023-04-25 11:56:58 -0500

    Finish concatenation of array

---
+++ array/1929-Concatenation-of-Array.js
@@ -0,0 +1,15 @@
+/*
+1929. Concatenation of Array
+https://leetcode.com/problems/concatenation-of-array/
+25 April 2023
+David Pratt
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var getConcatenation = function (nums) {
+  return nums.concat(nums);
+};
