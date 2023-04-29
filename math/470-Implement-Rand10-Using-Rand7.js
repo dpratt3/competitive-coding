b7886b74285496613d6c8885b1f3ba19c941ae4a
David Pratt <david@midnightmechanism.com>
2023-04-29 11:51:22 -0500

    Fix No. 470 to pass all test cases after designing algorithm

--- math/470-Implement-Rand10-Using-Rand7.js
+++ math/470-Implement-Rand10-Using-Rand7.js
@@ -12,7 +12,26 @@ David Pratt
  * @return {number} a random integer in the range 1 to 7
  */
 
+// Passes the test cases but is wrong!
+
+/*
 var rand10 = function() {
     return (rand7() + rand7() % 4);
 };
+*/
+
+// Correct solution
+
+var rand10 = function() {
+    let x = rand7();
+    let y = rand7();
+
+    // truncate sample space; dimension divisible by 10;
+    while(x > 6) x = rand7();
+    while(y > 5) y = rand7();
+
+    // divide sample space into 10 regions of three and return
+    if(x < 4) return y;
+    return y + 5;
+}
 
