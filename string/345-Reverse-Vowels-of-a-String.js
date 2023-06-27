61f66c0c0af9a0d8c4f7cf33d8db9a2f22c1c5cf
David Pratt <david@midnightmechanism.com>
2023-06-26 23:23:42 -0500

    345 Reverse Vowels of a String

---
+++ string/345-Reverse-Vowels-of-a-String.js
@@ -0,0 +1,29 @@
+/*
+345. Reverse Vowels of a String
+https://leetcode.com/problems/reverse-vowels-of-a-string/
+26 June 2023
+David Pratt
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var reverseVowels = function(s) {
+    const vowels = "aeiou"
+    let splitS = s.split('')
+    let filteredVowels = splitS.filter(x => vowels.includes(x)).reverse();
+
+    // fill in the original string with the reversed vowels
+    let counter = 0;
+    for(let i = 0; i < splitS.length; i+= 1){
+        if(vowels.includes(splitS[i])){
+            splitS[i] = filteredVowels[counter];
+            counter += 1;
+        }
+    }
+
+    return splitS.join('')
+};
+
