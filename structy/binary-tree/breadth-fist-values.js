c605a0800d310206911c699ea7aac2accbc30a8c
David Pratt <david@midnightmechanism.com>
2023-09-16 17:11:04 -0500

    Finish breadth first values

---
+++ structy/binary-tree/breadth-fist-values.js
@@ -0,0 +1,23 @@
+/*
+Anagrams
+https://structy.net/problems/premium/anagrams
+16 September 2023
+David Pratt
+*/
+
+const breadthFirstValues = (root) => {
+  // todo
+  
+  if(root === null) return []
+  const values = [];
+  const queue = [root];
+  
+  while(queue.length > 0){
+    const current = queue.shift();
+    values.push(current.val);
+    if(current.left !== null) queue.push(current.left);
+    if(current.right !== null) queue.push(current.right);
+  }
+   return values;
+}
+
